4ad27ef544ee26403143db65769485ec51bcf1b7
travis <2019103620@pnu.edu.ru>
2021-06-02 23:23:17 +0000

    Update doc from Travis CI

---
+++ html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gan',['GAN',['../matrixfact_8c.html#a977911494eba563e6f5e9f0538d701ee',1,'matrixfact.c']]]
+];
---
+++ html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['plus',['PLUS',['../matrixfact_8c.html#ac2abd2fbe58740e1af208d9ba4cd2cf3',1,'matrixfact.c']]]
+];
