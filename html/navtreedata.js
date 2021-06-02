8aa4cecc50331deb67ec47e0b58af886742d16a2
travis <2019103620@pnu.edu.ru>
2021-06-02 23:05:03 +0000

    Update doc from Travis CI

---
+++ html/navtreedata.js
@@ -0,0 +1,12 @@
+var NAVTREE =
+[
+  [ "Matrixx", "index.html", ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'нажмите на выключить для синхронизации панелей';
+var SYNCOFFMSG = 'нажмите на включить для синхронизации панелей';
