05a20ae7b2a4c11c845f37dafd91809ae91e7835
travis <2019103620@pnu.edu.ru>
2021-06-02 23:14:54 +0000

    Update doc from Travis CI

---
+++ html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['max',['MAX',['../matrixfact_8c.html#a5f421a374a567d1abdb81820d25c8a66',1,'matrixfact.c']]],
+  ['min',['MIN',['../matrixfact_8c.html#a114b4eac3dca10680ff5b9fc48d1f88f',1,'matrixfact.c']]]
+];
