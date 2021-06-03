f5eb15907986fa8c271b777dba7eda665e7663aa
ImyaPolzovatelyaPolzovatel <78893996+ImyaPolzovatelyaPolzovatel@users.noreply.github.com>
2021-06-03 12:41:27 +1000

    Delete html directory

--- html/search/all_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['matrixfact_2ec',['matrixfact.c',['../matrixfact_8c.html',1,'']]],
-  ['max',['MAX',['../matrixfact_8c.html#a5f421a374a567d1abdb81820d25c8a66',1,'matrixfact.c']]],
-  ['min',['MIN',['../matrixfact_8c.html#a114b4eac3dca10680ff5b9fc48d1f88f',1,'matrixfact.c']]]
-];
--- html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['plus',['PLUS',['../matrixfact_8c.html#ac2abd2fbe58740e1af208d9ba4cd2cf3',1,'matrixfact.c']]]
-];
